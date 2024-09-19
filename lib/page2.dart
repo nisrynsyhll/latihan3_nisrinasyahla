c1739f0ed9a3d6aae4c92060d6091341aa98e6aa
nisrynsyhll <nisrinamarhas@gmail.com>
2024-09-19 10:24:44 +0700

    Update page2.dart

--- lib/page2.dart
+++ lib/page2.dart
@@ -1,14 +1,50 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Page2 extends StatelessWidget {
   const Page2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('ini halaman 2')
-      )
-    );
-  }
+   return Scaffold(
+    body: Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.green,
+              ),
+            ],
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.blue,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.amber,
+                ),
+              ],
+            ),
+          ],
+         ),
+      ),
+     );
+   }
 }
+
